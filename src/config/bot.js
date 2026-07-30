896d33b609c4175315a8a4b5311cedb95a19c17d
m86618140-ops <m86618140@gmail.com>
2026-07-30 20:43:59 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "FlareX, Made by Diuox", // required by Discord API, not shown in the client
         state: "FlareX, Made by Diuox",     // this is what people actually see
         type: 4,               // Custom
       },
